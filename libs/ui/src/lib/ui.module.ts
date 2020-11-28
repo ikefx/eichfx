3bfd08f89b0ef2f08a2ca1356d94b41986868c25
Neil <n.eichelberger@gmail.com>
2020-11-28 15:32:13 -0600

    fixes

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './header/header.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MenuComponent, LandingPageComponent, HeaderComponent],
-  exports: [MenuComponent, LandingPageComponent, HeaderComponent],
+  declarations: [MenuComponent, LandingPageComponent, HeaderComponent, DisplayComponent],
+  exports: [MenuComponent, LandingPageComponent, HeaderComponent, DisplayComponent],
 })
 export class UiModule {}
