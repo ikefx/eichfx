bb5edbd5f35521863e8226f85de744b424d56864
Neil <n.eichelberger@gmail.com>
2020-11-28 19:15:37 -0600

    checkpoint

--- libs/ui/src/lib/menu/menu.component.ts
+++ libs/ui/src/lib/menu/menu.component.ts
@@ -9,6 +9,21 @@ export class MenuComponent implements OnInit {
 
   mockButtons: number[] = [1, 2, 3, 4, 5];
 
+  navMenu: any[] = [
+    {
+      label: 'Demo Reel',
+      route: 'demoReel'
+    },
+    {
+      label: 'Breakdown',
+      route: 'breakdown'
+    },
+    {
+      label: 'Research',
+      route: 'research'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
